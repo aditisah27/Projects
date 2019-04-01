8e3f3a04c6384df6926af45f7a12efa888620332
aditisah27 <aditi.sa2703@gmail.com>
2019-04-01 12:12:50 +0530

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from .views import PostListView , PostDetailView , PostCreateView , PostUpdateView
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostUpdateView.as_view(), name='post-update'),
+    path('about/', views.about, name='blog-about'),
+]
